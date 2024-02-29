a90d652cfa73f26a830ea508528fa0119e940041
FadlySatriadi <fadlisatriadi7@gmail.com>
2024-02-29 14:09:19 +0700

    Praktikum 2

---
+++ database/migrations/2024_02_29_070206_create_m_barang_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_barang', function (Blueprint $table) {
+            $table->id('barang_id');
+            $table->unsignedBigInteger('kategori_id')->index();
+            $table->string('barang_kode', 10)->unique();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_barang');
+    }
+};
