34a866fb04aba36740ace0926f1b17184b37ae45
FadlySatriadi <fadlisatriadi7@gmail.com>
2024-03-07 12:58:42 +0700

    Pertemuan 4 - Praktikum 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
         $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data);
-        
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];      
         $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
