33db3e11c1ced9a304783c88eb3efd11d4baf13f
FadlySatriadi <fadlisatriadi7@gmail.com>
2024-05-17 14:39:36 +0700

    Praktikum 3 - Jobsheet 10

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,6 @@ use App\Http\Controllers\Api\RegisterController;
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 Route::middleware('auth:api')->get('/user', function (Request $request){
-    
+    return $request->user();
 });
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
